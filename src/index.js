43ef6da4f971f560aa83f14bd5700c48f9831353
vanikkanen <vanikkanen@gmail.com>
2025-04-13 12:26:51 +0300

    Add weather API call function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import "./styles.css";
 
-console.log("Hello World!");
+const searchLocation = "Tampere";
+
+async function getCurrentWeather(location) {
+  try {
+    const weatherAPI = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&include=current&key=NTJQ6DFUHRNRWS36QK9MWYP46&contentType=json`;
+
+    const response = await fetch(weatherAPI, { mode: "cors" });
+    const weatherObj = await response.json();
+    console.log(weatherObj.currentConditions);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getCurrentWeather(searchLocation);
